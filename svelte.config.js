63798887d697d3ed3ca6bcb108e92debd3aae6ed
Florian Blaschke <florianblaschke@web.de>
2025-09-24 17:17:56 +0200

    change sizes and ttl

--- svelte.config.js
+++ svelte.config.js
@@ -9,8 +9,8 @@ const config = {
   kit: {
     adapter: adapter({
       images: {
-        sizes: [16, 32, 48, 64, 96, 128, 256, 384, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        minimumCacheTTL: 60,
+        sizes: [640, 828, 1200, 1920, 3840],
+        minimumCacheTTL: 300,
         domains: ["hochzeit-jp.vercel.app"],
         formats: ["image/avif", "image/webp"]
       }
