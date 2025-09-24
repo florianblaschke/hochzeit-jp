0d217f11e7c7e937ccfccf4c1e73e32601891020
Florian Blaschke <florianblaschke@web.de>
2025-09-24 09:12:47 +0200

    change routing, check for active session

--- src/lib/components/custom/navbar.svelte
+++ src/lib/components/custom/navbar.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { goto } from "$app/navigation";
     import { authClient } from "$lib/auth/client";
     import { Button } from "../ui/button";
     import ThemeSwitcher from "./theme-switcher.svelte";
@@ -8,15 +9,20 @@
 
 <header class="border-b border-border w-full">
     <div class="container mx-auto px-4 py-4 flex items-center justify-between">
-        <div class="flex-1"></div>
         <div class="flex justify-center gap-8 items-center">
-            <a href="/info"> Informationen </a>
-            <h1 class="text-2xl font-bold text-foreground">JP Wedding</h1>
-            <a href="/gallery">Gallerie</a>
+            <a href="/" class="font-bold text-lg"> JP Wedding </a>
+            {#if $session.data}
+                <a href="/gallery">Gallerie</a>
+            {/if}
         </div>
         <div class="flex-1 flex justify-end gap-4">
             {#if $session.data}
-                <Button onclick={() => authClient.signOut()}>Sign Out</Button>
+                <Button
+                    onclick={() => {
+                        authClient.signOut();
+                        goto("/login");
+                    }}>Sign Out</Button
+                >
             {/if}
             <ThemeSwitcher />
         </div>
