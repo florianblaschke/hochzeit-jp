20371d87ce52c17f0b1e8d7f7a3e95a26c477fa2
Florian Blaschke <florianblaschke@web.de>
2025-10-03 23:06:15 +0200

    hide sheet trigger on mobile if no session

--- src/lib/components/custom/navbar-mobile.svelte
+++ src/lib/components/custom/navbar-mobile.svelte
@@ -18,56 +18,60 @@
 
 <div class="flex items-center justify-between md:hidden px-4 py-4">
     <a href="/" class="font-bold text-lg"> JP Wedding </a>
-    <Sheet.Root>
-        <Sheet.Trigger
-            class={cn(
-                buttonVariants({
-                    variant: "outline",
-                }),
-            )}><Menu /></Sheet.Trigger
-        >
-        <Sheet.Content side="right">
-            <Sheet.Header>
-                <Sheet.Title>JP Wedding</Sheet.Title>
-                <Sheet.Description>
-                    {#if typeof attending !== "undefined"}
-                        <Badge
-                            variant="default"
-                            class={cn(
-                                attending ? "bg-green-500" : "bg-red-500",
-                            )}>{attending ? "Zugesagt" : "Abgesagt"}</Badge
+    {#if $session.data}
+        <Sheet.Root>
+            <Sheet.Trigger
+                class={cn(
+                    buttonVariants({
+                        variant: "outline",
+                    }),
+                )}><Menu /></Sheet.Trigger
+            >
+            <Sheet.Content side="right">
+                <Sheet.Header>
+                    <Sheet.Title>JP Wedding</Sheet.Title>
+                    <Sheet.Description>
+                        {#if typeof attending !== "undefined"}
+                            <Badge
+                                variant="default"
+                                class={cn(
+                                    attending ? "bg-green-500" : "bg-red-500",
+                                )}>{attending ? "Zugesagt" : "Abgesagt"}</Badge
+                            >
+                        {/if}
+                    </Sheet.Description>
+                </Sheet.Header>
+                <div class="grid flex-1 auto-rows-min gap-6 px-4">
+                    {#if $session.data}
+                        <a
+                            href="/gallery"
+                            class={cn(buttonVariants({ variant: "link" }))}
+                            >Gallerie</a
                         >
                     {/if}
-                </Sheet.Description>
-            </Sheet.Header>
-            <div class="grid flex-1 auto-rows-min gap-6 px-4">
-                {#if $session.data}
-                    <a
-                        href="/gallery"
-                        class={cn(buttonVariants({ variant: "link" }))}
-                        >Gallerie</a
-                    >
-                {/if}
-                {#if isAdmin}
-                    <a
-                        href="/list"
-                        class={cn(buttonVariants({ variant: "link" }))}
-                        >Guest List</a
-                    >
-                {/if}
-            </div>
+                    {#if isAdmin}
+                        <a
+                            href="/list"
+                            class={cn(buttonVariants({ variant: "link" }))}
+                            >Guest List</a
+                        >
+                    {/if}
+                </div>
 
-            <Sheet.Footer class="flex flex-row items-center justify-between">
-                {#if $session.data}
-                    <Button
-                        onclick={() => {
-                            authClient.signOut();
-                            goto("/login");
-                        }}>Sign Out</Button
-                    >
-                {/if}
-                <ThemeSwitcher />
-            </Sheet.Footer>
-        </Sheet.Content>
-    </Sheet.Root>
+                <Sheet.Footer
+                    class="flex flex-row items-center justify-between"
+                >
+                    {#if $session.data}
+                        <Button
+                            onclick={() => {
+                                authClient.signOut();
+                                goto("/login");
+                            }}>Sign Out</Button
+                        >
+                    {/if}
+                    <ThemeSwitcher />
+                </Sheet.Footer>
+            </Sheet.Content>
+        </Sheet.Root>
+    {/if}
 </div>
