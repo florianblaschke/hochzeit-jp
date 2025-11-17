753c30a6df00778973a3141663b27d0f673a1a3b
Florian Blaschke <florianblaschke@web.de>
2025-11-17 11:31:02 +0100

    cleanup

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,39 +1,8 @@
 <script lang="ts">
-    import { Button } from "$lib/components/ui/button";
-    import { Input } from "$lib/components/ui/input";
-    import { Label } from "$lib/components/ui/label";
-    import { cn } from "$lib/utils";
-    import { createMutation } from "@tanstack/svelte-query";
-    import LoginForm from "./login-form.svelte";
     import type { PageData } from "./$types";
+    import LoginForm from "./login-form.svelte";
 
     let { data }: { data: PageData } = $props();
-
-    // let loginMutation = createMutation({
-    //     mutationKey: ["login"],
-    //     mutationFn: async () => {
-    //         const res = await fetch(`/api/verify`, {
-    //             method: "POST",
-    //             headers: {
-    //                 "content-type": "application/json",
-    //             },
-    //             body: JSON.stringify({ email, password, name }),
-    //         });
-
-    //         if (res.status === 401) {
-    //             passwordRequired = true;
-    //             passwordMessage =
-    //                 "Bitte gib das Passwort auf der Einladung ein";
-    //             nameMessage = "Bitte gib einen Namen ein.";
-    //             throw new Error("missing_password");
-    //         }
-
-    //         if (res.status === 500) {
-    //             errorMessage =
-    //                 "Das Senden des Anmeldelinks ist fehlgeschlagen. Bitte versuche es erneut.";
-    //         }
-    //     },
-    // });
 </script>
 
 <div class="min-h-screen bg-background">
