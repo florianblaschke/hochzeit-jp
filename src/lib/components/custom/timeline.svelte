e77363f3cac6e5ebd4f132c4fc04fafedcb3974f
Florian Blaschke <florianblaschke@web.de>
2025-11-19 11:33:57 +0100

    change timeline

--- src/lib/components/custom/timeline.svelte
+++ src/lib/components/custom/timeline.svelte
@@ -19,20 +19,23 @@
         <h2 class="text-3xl font-bold mb-4">Ablauf</h2>
     </div>
 
-    <div class="max-w-2xl mx-auto">
-        <div class="space-y-6 grid items-center justify-center">
-            {#each timelineItems as item, index}
-                <div class="flex flex-col items-center gap-6">
+    <div class="max-w-4xl mx-auto w-full">
+        <div
+            class="flex items-center flex-col md:flex-row justify-center w-full divide-y md:divide-y-0 md:divide-x"
+        >
+            {#each timelineItems as item}
+                <div class="flex flex-1 flex-col items-center gap-6 p-6">
                     <div class="flex-shrink-0 w-20 text-right">
                         <span class="text-lg font-semibold text-primary"
                             >{item.time}</span
                         >
                     </div>
-                    <div class="flex-1">
-                        <h3 class="text-xl font-medium">{item.event}</h3>
+                    <div class="text-center">
+                        <h3 class="text-xl font-medium whitespace-nowrap">
+                            {item.event}
+                        </h3>
                     </div>
                 </div>
-                <Separator />
             {/each}
         </div>
     </div>
