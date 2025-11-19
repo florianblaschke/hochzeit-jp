38781cebdbc2a731a5ae0a1f6800b7ec06701d74
Florian Blaschke <florianblaschke@web.de>
2025-11-19 19:58:11 +0100

    add hero

---
+++ src/lib/components/custom/hero.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Image from "./image.svelte";
+</script>
+
+<div class="relative">
+    <Image
+        src="https://utfs.io/f/KCC3bUkGRI6aDr9H2BAsFpoxCthcJX0EdVaBSA6eiNk1QUMY"
+        class="grayscale-100"
+    />
+
+    <article
+        class="absolute bg-black/40 inset-0 z-10 flex flex-col items-center justify-center text-center"
+    >
+        <p class="text-white text-lg mb-2">18 | 04 | 2026</p>
+        <h1 class="text-white text-4xl md:text-6xl font-bold mb-2">
+            Jana & Philipp
+        </h1>
+        <p class="text-white text-xl">Wir heiraten</p>
+    </article>
+</div>
