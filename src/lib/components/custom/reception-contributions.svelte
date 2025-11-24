7d62df0fd242b79a5c1cd0906bcfa422d7f5d2f2
Florian Blaschke <florianblaschke@web.de>
2025-11-24 22:20:23 +0100

    change colors

--- src/lib/components/custom/reception-contributions.svelte
+++ src/lib/components/custom/reception-contributions.svelte
@@ -17,7 +17,7 @@
         class="container mx-auto flex flex-col items-stretch md:flex-row md:gap-12"
     >
         <article class="text-center mb-8 flex flex-col flex-1 items-center">
-            <h2 class="text-3xl font-bold mb-4">Sektempfang</h2>
+            <h3 class="text-3xl font-bold mb-4">Sektempfang</h3>
             <p class="text-muted-foreground max-w-2xl mx-auto mb-6 flex-1">
                 Wir freuen uns sehr, wenn ihr Häppchen, Kuchen oder Ähnliches
                 zum Sektempfang beisteuert. Bitte tragt euch dafür in die Liste
@@ -36,7 +36,7 @@
             </a>
         </article>
         <article class="text-center mb-8 flex flex-col flex-1 items-center">
-            <h2 class="text-3xl font-bold mb-4">Wunschliste</h2>
+            <h3 class="text-3xl font-bold mb-4">Wunschliste</h3>
             <p class="text-muted-foreground max-w-2xl mx-auto mb-6 flex-1">
                 Falls ihr uns eine Freude machen möchtet, haben wir eine kleine
                 Wunschliste zusammengestellt.
@@ -54,7 +54,7 @@
             </a>
         </article>
         <article class="text-center mb-8 flex flex-col flex-1 items-center">
-            <h2 class="text-3xl font-bold mb-4">Programm</h2>
+            <h3 class="text-3xl font-bold mb-4">Programm</h3>
             <p class="text-muted-foreground max-w-2xl mx-auto mb-6 flex-1">
                 Wir freuen uns sehr, wenn ihr den Abend aktiv mit gestaltet.
                 Meldet eure Beiträge dafür bitte bei Tobias Gritsch an.
